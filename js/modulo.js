a78feed21fdd24a89007686c81a4984a8f1a6793
Rene <rene.araujo@outlook.es>
2017-05-29 08:45:16 -0500

    JS mejorado con objetos

--- js/modulo.js
+++ js/modulo.js
@@ -6,10 +6,10 @@
 // 403: "Forbidden"
 // 404: "Page not found"
 // For a complete list go to the Http Messages Reference
-function insertRow(strDocument, strFullname, nScore) {
+function insertRow(rowData) {
     var image = document.createElement("img");
 
-    image.src = (nScore > 1600) ? "images/happy.png" : "images/unhappy.png";
+    image.src = rowData.score > 1600 ? "images/happy.png" : "images/unhappy.png";
     
     var table = document.getElementById("resultados");
     var row = table.insertRow(0);
@@ -20,9 +20,9 @@ function insertRow(strDocument, strFullname, nScore) {
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
 
-    cell2.appendChild(document.createTextNode(strDocument));
-    cell3.appendChild(document.createTextNode(strFullname));
-    cell4.appendChild(document.createTextNode(nScore));
+    cell2.appendChild(document.createTextNode(rowData.id));
+    cell3.appendChild(document.createTextNode(rowData.fullname));
+    cell4.appendChild(document.createTextNode(rowData.score));
     cell5.appendChild(image);
 }
 
@@ -30,7 +30,13 @@ function listener () {
     var data = JSON.parse(this.responseText);
 
     for(var i = 0; i < data.length; i++) {
-        insertRow(data[i].document, data[i].fullname, data[i].score);
+        var rd = {
+            id: data[i].document,
+            fullname: data[i].fullname,
+            score: data[i].score
+        };
+
+        insertRow(rd);
     }
 }
 
@@ -46,7 +52,8 @@ function cargaContenido() {
         console.log(xhr.status);
     };
 
-    xhr.open('GET', 'http://localhost/boot/data/estudiantes.json');
+    //xhr.open('GET', 'http://localhost/boot/data/estudiantes.json');
+    xhr.open('GET', 'https://raw.githubusercontent.com/ReneLeonidas/boot/master/data/estudiantes.json');
     // xhr.responseType = 'json';
 
     xhr.send();
